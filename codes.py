0b0aac6358d363a522f2c27cb7ccd0c5aba0db35
nelloworking7 <nelloworking7@gmail.com>
2025-07-27 18:00:58 +0900

    codes.py

--- codes.py
+++ codes.py
@@ -1,8 +1,40 @@
-subject_codes = {
-    1: "국어",
-    2: "수학",
-    3: "영어",
-    4: "한국사",
-    5: "탐구",
-    6: "제2외국어"
+SUBJECT_CODES = {
+    1: {
+        "name": "국어",
+        "type": "객관식",
+        "questions": list(range(1, 46))  # 1~45
+    },
+    2: {
+        "name": "수학",
+        "type": "혼합",  # 객관식 + 주관식
+        "questions": {
+            "객관식1": list(range(1, 16)),     # 1~15
+            "주관식1": list(range(16, 23)),    # 16~22
+            "객관식2": list(range(23, 29)),    # 23~28
+            "주관식2": list(range(29, 31))     # 29~30
+        }
+    },
+    3: {
+        "name": "영어",
+        "type": "객관식",
+        "questions": list(range(1, 46))  # 1~45
+    },
+    4: {
+        "name": "한국사",
+        "type": "객관식",
+        "questions": list(range(1, 21))  # 1~20
+    },
+    5: {
+        "name": "탐구",
+        "type": "복수과목",
+        "questions": {
+            "제1과목": list(range(1, 21)),    # 1~20
+            "제2과목": list(range(1, 21))     # 1~20
+        }
+    },
+    6: {
+        "name": "제2외국어",
+        "type": "객관식",
+        "questions": list(range(1, 31))  # 예시로 30문항
+    }
 }
